2b3ce207eb15efb2ee36a653aefe379a9ccc5968
Rares Andrei <andreirar24@gmail.com>
2020-02-27 11:05:51 -0500

    initial project version

---
+++ form_app/form_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+
+@app.route('/')
+def my_form():
+    return render_template('my_form.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    count = redis.incr(text)
+    return str(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
