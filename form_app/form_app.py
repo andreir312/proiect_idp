17d6aad1a2cf9cd2c853f56a6df1a586159dbd42
Rares Andrei <andreirar24@gmail.com>
2020-03-28 11:33:42 -0400

    Updated images

--- form_app/form_app.py
+++ form_app/form_app.py
@@ -2,20 +2,19 @@ from flask import Flask, request, render_template
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host="redis", port=6379)
 
 
-@app.route('/')
+@app.route("/")
 def my_form():
-    return render_template('my_form.html')
+    return render_template("my_form.html")
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def my_form_post():
-    text = request.form['text']
+    text = request.form["text"]
     count = redis.incr(text)
     return str(count)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
-
+    app.run(host="0.0.0.0", port=8000)
